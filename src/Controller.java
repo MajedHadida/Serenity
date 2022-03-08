5d22010e3bdd4ac1a465982a1a5d7f5d9b314bbc
Majed Hadida <majedhadida@hotmail.com>
2022-03-08 02:01:12 -0500

    first commit of Serenity

---
+++ src/Controller.java
@@ -0,0 +1,84 @@
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+public class Controller {
+
+    final ObservableList<String> THEMES = FXCollections.observableArrayList("Dark","Light");
+    
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private ComboBox<String> themeList;
+    @FXML
+    private BorderPane background;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Label incorrectLogin;
+
+
+    public void switchToLogin(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("loginScene.fxml"));
+        stage = (Stage)(((Node)event.getSource()).getScene().getWindow());
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public void switchToMainMenu(ActionEvent event) throws IOException{
+        if (checkLogin()){
+            root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+            stage = (Stage)(((Node)event.getSource()).getScene().getWindow());
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }else{
+            incorrectLogin.setOpacity(1f);
+        }
+        
+    }
+
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public boolean checkLogin(){
+        if  (username.getCharacters().toString().equals("username") && password.getCharacters().toString().equals("password")){
+            return true;
+        }
+        return false;
+    }
+
+    public void playAudio(){
+         String musicFile = "calm.mp3";     // For example
+         Media sound = new Media(new File(musicFile).toURI().toString());
+         MediaPlayer mediaPlayer = new MediaPlayer(sound);
+         mediaPlayer.play();
+     }
+
+
+}
---
+++ src/MenuController.java
@@ -0,0 +1,36 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+
+public class MenuController {
+    final ObservableList<String> THEMES = FXCollections.observableArrayList("Dark","Light");
+    @FXML
+    private ComboBox<String> themeList;
+    @FXML
+    private AnchorPane background;
+
+
+    @FXML
+    public void initialize() {
+        themeList.setItems(THEMES);
+        themeList.setValue("Light");
+    }
+
+    public void changeTheme(){
+        String theme = themeList.getValue();
+         switch (theme){
+            case "Dark":
+                background.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+                break;
+            case "Light":
+                background.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+                break;
+            }
+    }
+
+}
